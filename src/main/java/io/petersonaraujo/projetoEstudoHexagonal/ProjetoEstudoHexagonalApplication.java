049cae5fc0424656b31554a1f5c99361c1953b98
Peterson Araujo <petersonaraujo70@gmail.com>
2026-04-14 13:28:13 -0300

    feat: Criação do projeto

---
+++ src/main/java/io/petersonaraujo/projetoEstudoHexagonal/ProjetoEstudoHexagonalApplication.java
@@ -0,0 +1,13 @@
+package io.petersonaraujo.projetoEstudoHexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstudoHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstudoHexagonalApplication.class, args);
+	}
+
+}
